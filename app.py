b99e3009beed4c239e1ac24ba563d53f88108338
AhmadIbrahimMadaki <iahmadzakari@gmail.com>
2025-03-16 00:32:00 +0100

    remove sklearn

--- app.py
+++ app.py
@@ -190,9 +190,9 @@ from werkzeug.security import check_password_hash
 from sqlalchemy.exc import IntegrityError
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.preprocessing import StandardScaler
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
 
